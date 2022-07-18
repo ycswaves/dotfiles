dc9ee1f03609490769f97c0164569ade3c8bb9df
Chenshu Yi <cyi@abnormalsecurity.com>
2022-07-18 13:03:58 +0800

    update lvim config

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -4,7 +4,6 @@ lvim.format_on_save = true
 lvim.lsp.diagnostics.virtual_text = false
 lvim.colorscheme = "tokyonight"
 vim.g.tokyonight_style = "night"
--- vim.g.session_auto
 vim.g.fzf_layout = {
   window = {
     width = 0.85,
@@ -12,16 +11,7 @@ vim.g.fzf_layout = {
   }
 }
 -- vim.o.sessionoptions="blank,buffers,curdir,folds,help,options,tabpages,winsize,resize,winpos,terminal"
-vim.o.sessionoptions="curdir,folds,help,options,tabpages,winsize,resize,winpos,terminal"
--- vim.g.bufferline = {
---   maximum_length = 120,
--- }
-
--- lvim.builtin.bufferline.insert_at_end = false
-
--- lvim.colorscheme = "onedark"
--- vim.g.material_style = "deep ocean"
-
+vim.o.sessionoptions = "curdir,folds,help,options,tabpages,winsize,resize,winpos,terminal"
 vim.opt.cmdheight = 1 -- more space in the neovim command line for displaying messages
 vim.opt.relativenumber = true
 
@@ -32,12 +22,21 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.dashboard.active = true
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.notify.active = true
 lvim.builtin.nvimtree.setup.view.width = 50
 
+
+lvim.builtin.project.detection_methods = {
+  "pattern",
+  "lsp",
+}
+lvim.builtin.project.patterns = {
+  "package.json",
+  ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile",
+}
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
@@ -70,7 +69,7 @@ lvim.keys.normal_mode = {
   ["<C-j>"] = "<C-w>j",
   ["<C-k>"] = "<C-w>k",
   ["<C-l>"] = "<C-w>l",
-  ["<C-p>"] = ":Telescope find_files<CR>",
+  ["<C-p>"] = ":Telescope git_files<CR>",
 
   -- Resize with arrows
   ["<C-Up>"] = ":resize -2<CR>",
@@ -78,18 +77,25 @@ lvim.keys.normal_mode = {
   ["<C-Left>"] = ":vertical resize -2<CR>",
   ["<C-Right>"] = ":vertical resize +2<CR>",
 
-   -- Navigate buffers
-   ["<S-Tab>"] = ":BufferLineCyclePrev<CR>",
-   ["<Tab>"] = ":BufferLineCycleNext<CR>",
+  -- Navigate buffers
+  ["<S-Tab>"] = ":BufferLineCyclePrev<CR>",
+  ["<Tab>"] = ":BufferLineCycleNext<CR>",
 
- }
+}
 
-lvim.builtin.which_key.mappings["*"] = { "<Plug>(FerretAckWord)", "FerretAckWord" }
-lvim.builtin.which_key.mappings["f"] = { "<Plug>(FerretBack)", "Search in file" }
-lvim.builtin.which_key.mappings["f"] = { "<Plug>(FerretBack)", "Search in file" }
+lvim.builtin.which_key.mappings["*"] = { "<cmd>Telescope grep_string<cr>", "Search string under cursor" }
+lvim.builtin.which_key.mappings["b"]["f"] = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Search in current buffer" }
+lvim.builtin.which_key.mappings["b"]["s"] = { "<cmd>Buffers<cr>", "Search Buffers" }
+lvim.builtin.which_key.mappings["b"]["o"] = { "<cmd>%bd|e#|bd#<cr>", "Current Buffer Only" }
+lvim.builtin.which_key.mappings["b"]["c"] = { "<cmd>b#|bd#<cr>", "Close Buffer" }
 lvim.builtin.which_key.mappings["o"] = { "<cmd>GBrowse!<cr>", "Git Browse!" }
 lvim.builtin.which_key.mappings["g"]["l"] = { "<cmd>Git blame<cr>", "Git Blame!" }
+lvim.builtin.which_key.mappings["g"]["S"] = { "<cmd>Git<cr>", "Git Status" }
 lvim.builtin.which_key.mappings["l"]["o"] = { "<cmd>SymbolsOutline<cr>", "Symbols Outline" }
+lvim.builtin.which_key.mappings["dU"] = { "<cmd>lua require('dapui').toggle()<cr>", "DAP UI" }
+lvim.builtin.which_key.mappings["ag"] = { "<cmd>call fzf#vim#ag(expand('<cword>'), fzf#vim#with_preview())<cr>", "Search under cursor" }
+lvim.builtin.which_key.mappings["wa"] = { "<cmd>:wa<cr>", "Save all" }
+
 
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
@@ -97,40 +103,14 @@ formatters.setup {
   { exe = "gofmt" },
   {
     exe = "prettier",
-    -- filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
   },
 }
 
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  -- { exe = "flake8" },
-  {
-    exe = "eslint_d",
-    -- filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-  }
-}
-
-local util = require("lspconfig/util")
-lvim.lang.ruby.lsp.setup = {
-    cmd = { "solargraph", "stdio" },
-    filetypes = { "ruby" },
-    init_options = {
-      formatting = true
-    },
-    root_dir = util.root_pattern("Gemfile", ".git"),
-    settings = {
-      solargraph = {
-        diagnostics = false
-      }
-    }
-}
-
-
 lvim.plugins = {
-  {"lunarvim/colorschemes"},
-  {"prettier/vim-prettier"},
-  {"marko-cerovac/material.nvim"},
-  {'yashguptaz/calvera-dark.nvim'},
+  { "lunarvim/colorschemes" },
+  { "prettier/vim-prettier" },
+  { "marko-cerovac/material.nvim" },
+  { 'yashguptaz/calvera-dark.nvim' },
   -- {
   --   "sunjon/shade.nvim",
   --   config = function ()
@@ -140,10 +120,11 @@ lvim.plugins = {
   --     })
   --   end
   -- },
-  {"tpope/vim-surround"},
-  {"tpope/vim-fugitive"},
+  { "tpope/vim-surround" },
+  { "tpope/vim-fugitive" },
   -- required by fugitive to :Gbrowse
-  {'tpope/vim-rhubarb'},
+  { 'tpope/vim-rhubarb' },
+  { 'tpope/vim-repeat' },
   {
     "norcalli/nvim-colorizer.lua",
     config = function()
@@ -158,37 +139,32 @@ lvim.plugins = {
       })
     end,
   },
-  {'simrat39/symbols-outline.nvim'},
-  {"folke/tokyonight.nvim"},
+  { 'simrat39/symbols-outline.nvim' },
+  { "folke/tokyonight.nvim" },
   {
     'lukas-reineke/indent-blankline.nvim',
     config = function()
       require("indent_blankline").setup {
         char = "│",
         -- let g:indent_blankline_char_list = ['|', '¦', '┆', '┊']
-        filetype_exclude = {"dashboard", "markdown"},
-        buftype_exclude = {"terminal", "nofile"},
+        filetype_exclude = { "dashboard", "markdown" },
+        buftype_exclude = { "terminal", "nofile" },
         use_treesitter = true
       }
     end,
   },
   {
     'rmagatti/goto-preview',
-    config = function()
-      require('goto-preview').setup({
-        width = 135; -- Width of the floating window
-        height = 20; -- Height of the floating window
-        -- default_mappings = false; -- Bind default mappings
-        debug = false; -- Print debug information
-        opacity = nil; -- 0-100 opacity level of the floating window where 100 is fully transparent.
-        post_open_hook = nil -- A function taking two arguments, a buffer and a window to be ran as a hook.
-      })
-    end
   },
-  {'wincent/ferret'},
-  {'junegunn/fzf'},
-  {'junegunn/fzf.vim'},
-  -- {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
-  -- {'xolox/vim-misc'},
-  -- {'xolox/vim-session'},
+  { 'junegunn/fzf' },
+  { 'junegunn/fzf.vim' },
+  { 'rcarriga/nvim-dap-ui', requires = { 'mfussenegger/nvim-dap' } },
 }
+
+-- lvim.builtin.dap.active = true
+-- local dap_install = require("dap-install")
+-- dap_install.config("go_delve", {})
+
+-- require("dapui").setup()
+
+require("lvim.lsp.manager").setup("eslint")
