ac8c6789e606c04707f7ea3098d8dd379f03aa47
Chenshu Yi <cyi@abnormalsecurity.com>
2023-05-08 12:04:12 +0800

    update config before upgrade lunarvim

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -1,7 +1,7 @@
 -- general
 lvim.log.level = "warn"
-lvim.format_on_save = true
-lvim.lsp.diagnostics.virtual_text = false
+lvim.format_on_save = { timeout = 10000 }
+-- lvim.lsp.diagnostics.virtual_text = false
 -- lvim.colorscheme = "catppuccin"
 -- vim.g.catppuccin_flavour = "mocha"
 lvim.colorscheme = "tokyonight"
@@ -33,6 +33,12 @@ lvim.builtin.telescope = {
   active = true,
   defaults = {
     layout_strategy = "horizontal",
+  },
+}
+
+lvim.lsp.null_ls.setup = {
+  log = {
+    level = "info",
   },
 }
 
@@ -79,17 +85,14 @@ lvim.keys.normal_mode = {
   ["<C-k>"] = "<C-w>k",
   ["<C-l>"] = "<C-w>l",
   ["<C-p>"] = ":Telescope find_files<CR>",
-
   -- Resize with arrows
   ["<C-Up>"] = ":resize -2<CR>",
   ["<C-Down>"] = ":resize +2<CR>",
   ["<C-Left>"] = ":vertical resize -2<CR>",
   ["<C-Right>"] = ":vertical resize +2<CR>",
-
   -- Navigate buffers
   ["<S-Tab>"] = ":BufferLineCyclePrev<CR>",
   ["<Tab>"] = ":BufferLineCycleNext<CR>",
-
 }
 
 lvim.builtin.which_key.mappings["*"] = { "<cmd>Telescope grep_string<cr>", "Search string under cursor" }
@@ -101,10 +104,11 @@ lvim.builtin.which_key.mappings["o"] = { "<cmd>GBrowse!<cr>", "Git Browse!" }
 lvim.builtin.which_key.mappings["g"]["l"] = { "<cmd>Git blame<cr>", "Git Blame!" }
 lvim.builtin.which_key.mappings["g"]["S"] = { "<cmd>Git<cr>", "Git Status" }
 lvim.builtin.which_key.mappings["dU"] = { "<cmd>lua require('dapui').toggle()<cr>", "DAP UI" }
-lvim.builtin.which_key.mappings["ag"] = { "<cmd>call fzf#vim#ag(expand('<cword>'), '--ignore node_modules', fzf#vim#with_preview())<cr>",
+lvim.builtin.which_key.mappings["ag"] = {
+  "<cmd>call fzf#vim#ag(expand('<cword>'), '--ignore node_modules', fzf#vim#with_preview())<cr>",
   "Search under cursor" }
 lvim.builtin.which_key.mappings["wa"] = { "<cmd>:wa<cr>", "Save all" }
-
+lvim.builtin.which_key.mappings["ai"] = { "<cmd>:Copilot panel<cr>", "Copilot" }
 
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
@@ -112,9 +116,11 @@ formatters.setup {
   { exe = "gofmt" },
   { exe = "eslint_d" },
   {
-    exe = "prettier",
+    exe = "prettierd",
   },
 }
+
+vim.b.copilot_suggestion_hidden = false
 
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
@@ -134,7 +140,7 @@ code_actions.setup {
 }
 
 lvim.plugins = {
-  { "catppuccin/nvim", as = "catppuccin" },
+  { "catppuccin/nvim",   as = "catppuccin" },
   -- {
   --   "sunjon/shade.nvim",
   --   config = function ()
@@ -154,13 +160,13 @@ lvim.plugins = {
     "norcalli/nvim-colorizer.lua",
     config = function()
       require("colorizer").setup({ "*" }, {
-        RGB = true, -- #RGB hex codes
-        RRGGBB = true, -- #RRGGBB hex codes
+        RGB = true,      -- #RGB hex codes
+        RRGGBB = true,   -- #RRGGBB hex codes
         RRGGBBAA = true, -- #RRGGBBAA hex codes
-        rgb_fn = true, -- CSS rgb() and rgba() functions
-        hsl_fn = true, -- CSS hsl() and hsla() functions
-        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        rgb_fn = true,   -- CSS rgb() and rgba() functions
+        hsl_fn = true,   -- CSS hsl() and hsla() functions
+        css = true,      -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = true,   -- Enable all CSS *functions*: rgb_fn, hsl_fn
       })
     end,
   },
@@ -172,10 +178,50 @@ lvim.plugins = {
   { 'junegunn/fzf.vim' },
   { 'nvim-treesitter/nvim-treesitter-context' },
   { 'nvim-treesitter/nvim-treesitter-textobjects' },
-  { 'karb94/neoscroll.nvim',
-    require('neoscroll').setup(
-      { mappings = { '<C-u>', '<C-d>', '<C-b>',
-        '<C-y>', '<C-e>', 'zt', 'zz', 'zb' }, }
-    )
+  -- { 'github/copilot.vim', branch = 'release' },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        -- suggestion = { enabled = false },
+        -- panel = { enabled = false },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = "<C-l>",
+            accept_word = false,
+            accept_line = false,
+            next = "<ALT-]>",
+            prev = "<ALT-[>",
+            dismiss = "<C-]>",
+          },
+        },
+      })
+    end
   },
+  -- {
+  --   "zbirenbaum/copilot-cmp",
+  --   after = { "copilot.lua" },
+  --   config = function()
+  --     require("copilot_cmp").setup({
+  --       method = "getCompletionsCycling",
+  --       formatters = {
+  --         label = require("copilot_cmp.format").format_label_text,
+  --         insert_text = require("copilot_cmp.format").format_insert_text,
+  --         preview = require("copilot_cmp.format").deindent,
+  --       },
+  --     })
+  --   end
+  -- },
+  -- { 'karb94/neoscroll.nvim',
+  --   require('neoscroll').setup(
+  --     { mappings = { '<C-u>', '<C-d>', '<C-b>',
+  --       '<C-y>', '<C-e>', 'zt', 'zz', 'zb' }, }
+  --   )
+  -- },
 }
+require("lvim.lsp.manager").setup("emmet_ls")
